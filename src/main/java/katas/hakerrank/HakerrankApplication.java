c201d8c4e2cbbb43b3a729441babef7e265c68bf
Carolina Nichita <carolinanichita89@gmail.com>
2023-09-20 05:06:02 +0200

    setup project

---
+++ src/main/java/katas/hakerrank/HakerrankApplication.java
@@ -0,0 +1,13 @@
+package katas.hakerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakerrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakerrankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/katas/hakerrank/HakerrankApplicationTests.java
@@ -0,0 +1,13 @@
+package katas.hakerrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakerrankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
