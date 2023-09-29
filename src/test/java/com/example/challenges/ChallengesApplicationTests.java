a12b83c2e5eac2f88002b9aebaf33e1b9b68bd79
Carolina Nichita <carolinanichita89@gmail.com>
2023-09-29 12:03:08 +0200

    last challenges

--- src/test/java/com/example/challenges/ChallengesApplicationTests.java
+++ src/test/java/com/example/challenges/ChallengesApplicationTests.java
@@ -1,4 +1,4 @@
-package hakerank.challenges;
+package com.example.challenges;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChallengesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
