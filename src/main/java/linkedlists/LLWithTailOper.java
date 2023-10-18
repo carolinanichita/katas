c4cfadcf1a36edd791cb57c9e6d91b9810580346
Carolina Nichita <carolinanichita89@gmail.com>
2023-10-18 11:21:58 +0200

    insertion and deletion at the end of a doubly linked list

---
+++ src/main/java/linkedlists/LLWithTailOper.java
@@ -0,0 +1,97 @@
+package linkedlists;
+
+public class LLWithTailOper<T> {
+    public class Node {
+        public T data;
+        public Node nextNode;
+        public Node prevNode;
+    }
+
+    public Node headNode;
+    public Node tailNode;
+    public int size;
+
+    public LLWithTailOper() {
+        this.headNode = null;
+        this.tailNode = null; //null initially
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        if (headNode == null && tailNode == null) //checking tailNode to make sure
+            return true;
+        return false;
+    }
+
+    public Node getHeadNode() {
+        return headNode;
+    }
+
+    public Node getTailNode() {
+        return tailNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void insertAtHead(T data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = this.headNode; //Linking newNode to head's nextNode
+        newNode.prevNode = null; //it will be inserted at start so prevNode will be null
+        if (!isEmpty())
+            headNode.prevNode = newNode;
+        else
+            tailNode = newNode;
+        this.headNode = newNode;
+        size++;
+    }
+
+    public void insertAtEnd(T data) {
+        if (isEmpty()) { //if list is empty then insert at head
+            insertAtHead(data);
+            return;
+        }
+        //make a new node and assign it the value to be inserted
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = null; //it will be inserted at end so nextNode will be null
+        newNode.prevNode = tailNode; //newNode comes after tailNode so its prevNode will be tailNode
+        tailNode.nextNode = newNode; //make newNode the nextNode of tailNode
+        tailNode = newNode; //update tailNode to be the newNode
+        size++;
+    }
+
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        Node temp = headNode;
+        System.out.print("List : null <- ");
+
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.nextNode;
+        }
+
+        System.out.println(temp.data.toString() + " -> null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedListOperations<Integer> list = new DoublyLinkedListOperations<Integer>();
+        for (int i = 0; i <= 5; i++) {
+            list.insertAtHead(i);
+        }
+        System.out.println("List after inserting from 0 to 5: ");
+        list.printList();
+
+        for (int i = 7; i <= 12; i++) {
+            list.insertAtEnd(i);
+        }
+        System.out.println("List after inserting from 7 to 12 at the end");
+
+    }
+}
