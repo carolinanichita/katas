990c02409861f09254255fc11a1cb699cec6ecd3
Carolina Nichita <carolinanichita89@gmail.com>
2023-10-24 00:29:31 +0200

    reversed array of characters with tests

---
+++ src/test/java/org/arrays/ReverseArrOfCharTest.java
@@ -0,0 +1,16 @@
+package org.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseArrOfCharTest {
+    @Test
+    public void reverseArrayOfCharacters() {
+        char[] arrayToReverse = {'c', 'a', 'r', 'o', 'l', 'i', 'n', 'a'};
+        char[] reversed = ReverseArrOfChar.reverseArray(arrayToReverse);
+        char[] expected = {'a', 'n', 'i', 'l', 'o', 'r', 'a', 'c'};
+        assertArrayEquals(expected, reversed);
+    }
+
+}
