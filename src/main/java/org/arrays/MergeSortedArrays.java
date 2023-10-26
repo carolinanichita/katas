e262127c377503ff939823bc1718f75eb4a847a3
Carolina Nichita <carolinanichita89@gmail.com>
2023-10-26 10:47:47 +0200

    merged arrays improved

--- src/main/java/org/arrays/MergeSortedArrays.java
+++ src/main/java/org/arrays/MergeSortedArrays.java
@@ -8,31 +8,20 @@ public class MergeSortedArrays {
         int currentIndexMergedArr = 0;
 
         while (currentIndexMergedArr < mergedArray.length) {
-            if (currentIndexArr1 >= arr1.length) {
-                mergedArray[currentIndexMergedArr] = arr2[currentIndexArr2];
-                currentIndexArr2++;
-            } else if (currentIndexArr2 >= arr2.length){
-                mergedArray[currentIndexMergedArr] = arr1[currentIndexArr1];
-                currentIndexArr1++;
-            } else if (arr1[currentIndexArr1] < arr2[currentIndexArr2]) {
+
+            boolean isArr1Exhausted = currentIndexArr1 >= arr1.length;
+            boolean isArr2Exhausted = currentIndexArr2 >= arr2.length;
+
+            if (!isArr1Exhausted && (isArr2Exhausted || (arr1[currentIndexArr1] < arr2[currentIndexArr2]))) {
                 mergedArray[currentIndexMergedArr] = arr1[currentIndexArr1];
                 currentIndexArr1++;
             } else {
                 mergedArray[currentIndexMergedArr] = arr2[currentIndexArr2];
                 currentIndexArr2++;
             }
-
             currentIndexMergedArr++;
         }
 
         return mergedArray;
-    }
-
-    public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5};
-        int[] arr2 = {2, 4, 6};
-
-        int[] actual = MergeSortedArrays.mergeSortArrays(arr1, arr2);
-        System.out.println(actual);
     }
 }
