3198ab4813e88cf2ea977a6d9fa8f6b5c85986fa
Carolina Nichita <carolinanichita89@gmail.com>
2023-11-02 22:43:46 +0100

    a better solution with O(n) time and O(n) space costs

--- src/test/java/meeting_ranges/FirstComeFirstServedTest.java
+++ src/test/java/meeting_ranges/FirstComeFirstServedTest.java
@@ -12,7 +12,7 @@ class FirstComeFirstServedTest {
         final int[] dineInOrders = {4,5,6};
         final int[] servedOrders = {1,2,3,4,5,6};
 
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertTrue(result);
     }
 
@@ -22,7 +22,7 @@ class FirstComeFirstServedTest {
         final int[] dineInOrders = {4,5,6,7,9,11};
         final int[] servedOrders = {1,2,3,4,5,11,7,9,6};
 
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertFalse(result);
     }
 
@@ -32,7 +32,7 @@ class FirstComeFirstServedTest {
         final int[] dineInOrders = {4,5,6,7,9,11};
         final int[] servedOrders = {4,5,6,7,9,11};
 
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertTrue(result);
     }
 
@@ -42,7 +42,7 @@ class FirstComeFirstServedTest {
         final int[] dineInOrders = {4,5,6,7,9};
         final int[] servedOrders = {1,2,9,11,5,4,7,3,6};
 
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders,dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders,dineInOrders, servedOrders,0,0,0);
         assertFalse(result);
     }
 
@@ -51,7 +51,7 @@ class FirstComeFirstServedTest {
         final int[] takeOutOrders = {1, 9};
         final int[] dineInOrders = {7, 8};
         final int[] servedOrders = {1, 7, 8};
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertFalse(result);
     }
 
@@ -60,7 +60,7 @@ class FirstComeFirstServedTest {
         final int[] takeOutOrders = {55, 9};
         final int[] dineInOrders = {7, 8};
         final int[] servedOrders = {1, 7, 8, 9};
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertFalse(result);
     }
 
@@ -69,7 +69,7 @@ class FirstComeFirstServedTest {
         final int[] takeOutOrders = {27, 12, 18};
         final int[] dineInOrders = {55, 31, 8};
         final int[] servedOrders = {55, 31, 8, 27, 12, 18};
-        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+        final boolean result = FirstComeFirstServed.isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders,0,0,0);
         assertTrue(result);
     }
 
